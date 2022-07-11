d250a5d200e3b6b816bdc73888c1b914a7ee6fbe
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2022-07-11 22:08:54 +0800

    fixed shopview

--- PokemonRaid/Assets/Scripts/Shop/ShopView.cs
+++ PokemonRaid/Assets/Scripts/Shop/ShopView.cs
@@ -1,9 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Shop
 {
     public class ShopView : MonoBehaviour
     {
-        
+        [SerializeField] private Button _purchaseMeleeButton;
+        [SerializeField] private Button _purchaseRangedButton;
+
+        public event Action PurchaseMeleeButtonPressed;
+        public event Action PurchaseRangedButtonPressed;
+
+        private void Start()
+        {
+            _purchaseMeleeButton.onClick.AddListener(OnPurchaseMeleeButtonClicked);
+            _purchaseRangedButton.onClick.AddListener(OnPurchaseRangedButtonClicked);
+        }
+        private void OnDestroy()
+        {
+            _purchaseMeleeButton.onClick.RemoveListener(OnPurchaseMeleeButtonClicked);
+            _purchaseRangedButton.onClick.RemoveListener(OnPurchaseRangedButtonClicked);
+        }
+
+        protected  void  OnPurchaseMeleeButtonClicked()
+        {
+            PurchaseMeleeButtonPressed?.Invoke();
+        }
+        protected  void  OnPurchaseRangedButtonClicked()
+        {
+            PurchaseRangedButtonPressed?.Invoke();
+        }
     }
 }
