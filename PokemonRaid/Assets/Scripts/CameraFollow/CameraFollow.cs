91c9e53949458e2762a3c71b3116c689289f3b6c
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2022-07-14 22:55:21 +0800

    Added camerafollow

---
+++ PokemonRaid/Assets/Scripts/CameraFollow/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace CameraFollow
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private PlayerView _playerView;
+
+        private Vector3 _offset = new Vector3(0, 18, -6.4f);
+
+        private void LateUpdate()
+        {
+            transform.position = _playerView.transform.position + _offset;
+        }
+    }
+}
